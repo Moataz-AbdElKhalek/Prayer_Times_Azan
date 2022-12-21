bb39ca5c9651b7b9ab63a61e3f7cbba6c08bf840
Moataz-AbdElKhalek <Moataz-AbdElKhalek@users.noreply.github.com>
2022-12-21 17:09:49 -0600

    Fixing schedule several instances issue.

--- src/azan.sh
+++ src/azan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://www.arqum.org/ |  grep -i "<td>" | sed 's/          <td>//' | sed 's/<\/td>//' | sed 's/ //' | sed -e "s/\r//g" > azan.txt
+curl -s https://www.arqum.org/ |  grep -i "<td>" | sed 's/          <td>//' | sed 's/<\/td>//' | sed 's/ //' | sed -e "s/\r//g" > <full_path>/prayer_times_azan/azan.txt
 
 fajr=$(sed '2!d' azan.txt)
 
@@ -12,20 +12,20 @@ maghrib=$(sed '11!d' azan.txt)
 
 isha=$(sed '14!d' azan.txt)
 
-
-echo Fajr is $fajr
-echo Zuhr is $zuhr
-echo Asr is $asr
-echo Maghrib is $maghrib
-echo Isha is $isha
+echo Fajr    $fajr
+echo Zuhr    $zuhr
+echo Asr     $asr
+echo Maghrib $maghrib
+echo Isha    $isha
 
 # Remove all at queue:
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 
-at $fajr <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
-at $zuhr <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
-at $asr <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
-at $maghrib <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
-at $isha <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
+at $fajr <<< "cvlc --play-and-exit <full_path>/prayer_times_azan/azan.mp3; <full_path>/prayer_times_azan/azan.sh"
+at $zuhr <<< "cvlc --play-and-exit <full_path>/prayer_times_azan/azan.mp3; <full_path>/prayer_times_azan/azan.sh"
+at $asr <<< "cvlc --play-and-exit <full_path>/prayer_times_azan/azan.mp3; <full_path>/prayer_times_azan/azan.sh"
+at $maghrib <<< "cvlc --play-and-exit <full_path>/prayer_times_azan/azan.mp3; <full_path>/prayer_times_azan/azan.sh"
+at $isha <<< "cvlc --play-and-exit <full_path>/prayer_times_azan/azan.mp3; <full_path>/prayer_times_azan/azan.sh"
 
-#at $isha-10 minutes <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan\ app/azan.mp3"
+#at $isha-10 minutes <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan_app/azan.mp3"
+#at now+1 minute <<< "cvlc --play-and-exit /home/mad/MAD_Labs/azan_app/moo.mp3; /home/mad/MAD_Labs/azan_app/azan.sh"
